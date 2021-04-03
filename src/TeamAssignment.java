0a8004fc9b2f0e109b81a5fb5d26e96a9f9d6aaf
Abdulhaluk akpinar <haluk126@Abdulhaluks-MacBook-Pro.local>
2021-04-03 16:03:22 -0400

    answer for boolean

--- src/TeamAssignment.java
+++ src/TeamAssignment.java
@@ -1,29 +1,37 @@
 import java.util.*;
 
 public class TeamAssignment {
-
-    // method to print out all values in a List<String> void printValues(List<String> list) {}
-
+    public static void main(String[] args) {
 
 
-    // method to add all the numbers in a List<Integer>: int sum(List<Integer> list) {}
+        // method to print out all values in a List<String> void printValues(List<String> list) {}
 
 
-
-    // method to find the index of a value in a String[]: int getIndex(String[] arr, String value) {}
+        // method to add all the numbers in a List<Integer>: int sum(List<Integer> list) {}
 
 
 
-    // method to multiply two int together: int multiply(int a, int b) {}
+        // method to find the index of a value in a String[]: int getIndex(String[] arr, String value) {}
 
 
 
-    // method to tell if an int is even: boolean isEven(int number) {}
+        // method to multiply two int together: int multiply(int a, int b) {}
 
 
 
-    // method to print hello world: void helloWorld() {}
+        // method to tell if an int is even: boolean isEven(int number) {}
+        public stati void main{
+            int x = in.nextInt();
+            boolean even;
+            for (int i = 0; i == 5; i++) {
+                if ((x / 2) * 2 == x) {
+                    even = true;
+                    System.out.println(x + " is even.");
+                }
+        }
+
+        // method to print hello world: void helloWorld() {}
 
 
-
+    }
 }
